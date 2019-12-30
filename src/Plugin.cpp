a27a2763ac22930cef6e46d1641343d28d98a293
JanHolger <jan@bebendorf.eu>
2019-12-30 17:37:22 +0100

    tried to fix linux

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -2,14 +2,9 @@
 #include <iostream>
 #include <cstring>
 #include <string>
-#ifdef _WIN32
+
 #include <filesystem>
 namespace fs = std::filesystem;
-#else
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#endif
-
 
 #ifdef _WIN32
 	#include <windows.h>
