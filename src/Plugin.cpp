285e24253053d9a1e2352659427bb5f1e1203697
JanHolger <jan@bebendorf.eu>
2020-07-10 00:33:02 +0200

    Added debugging for CallLuaFunction

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -22,12 +22,10 @@ namespace fs = std::filesystem;
 #endif
 
 Lua::LuaArgs_t CallLuaFunction(lua_State* ScriptVM, const char* LuaFunctionName, Lua::LuaArgs_t* Arguments) {
-	Onset::Plugin::Get()->Log("Function Name: %s", LuaFunctionName);
-	Onset::Plugin::Get()->Log("Function Arg Count: %i", Arguments->size());
-	Lua::LuaArgs_t arg_list;
-	Lua::ParseArguments(ScriptVM, arg_list);
-	int arg_size = static_cast<int>(arg_list.size());
-	Onset::Plugin::Get()->Log("Stack: %i", arg_size);
+	Lua::LuaArgs_t stack;
+	Lua::ParseArguments(ScriptVM, stack);
+	int stack_size = static_cast<int>(stack.size());
+	lua_pop(L, stack_size);
 	Lua::LuaArgs_t ReturnValues;
 	int ArgCount = lua_gettop(ScriptVM);
 	lua_getglobal(ScriptVM, LuaFunctionName);
